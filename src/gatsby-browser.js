fa023f6501d219c3900c4f446f9d20eb69ec5f8a
Kennedy Rose <kennedy@kennedyrose.com>
2018-02-22 14:09:50 -0600

    Updated build

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,3 +1,8 @@
-exports.onInitialClientRender = () => {
-	console.log('Do something')
+export function onRouteUpdate({ location }) {
+	if (process.env.NODE_ENV === 'production' && typeof window._hsq === 'object') {
+		// Wait for the title update (see #2478)
+		setTimeout(() => {
+			window._hsq.push(['trackPageView'])
+		}, 0)
+	}
 }
